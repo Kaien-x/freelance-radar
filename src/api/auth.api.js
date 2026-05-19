cb8c4b8165f8961c2258d58135adc3869dcc71da
Kaien-x <manvendrasingh7036@gmail.com>
2026-05-20 01:07:32 +0530

    fix api url

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,6 +1,23 @@
 import api from './client';
-export const loginAPI = (data) => api.post('/auth/login', data);
-export const registerAPI = (data) => api.post('/auth/register', data);
+
+export const loginAPI = (data) => {
+  console.log(api.defaults.baseURL);
+  console.log('LOGIN API CALLED');
+
+  return api.post('/auth/login', data);
+};
+
+export const registerAPI = (data) => {
+  console.log(api.defaults.baseURL);
+  console.log('REGISTER API CALLED');
+
+  return api.post('/auth/register', data);
+};
+
 export const getMeAPI = () => api.get('/auth/me');
-export const updateProfileAPI = (data) => api.put('/auth/profile', data);
-export const updateSkillsAPI = (skills) => api.put('/auth/skills', { skills });
+
+export const updateProfileAPI = (data) =>
+  api.put('/auth/profile', data);
+
+export const updateSkillsAPI = (skills) =>
+  api.put('/auth/skills', { skills });
--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+console.log('API URL:', import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://43.205.113.12:8008/api',
   // Don't set default Content-Type to allow FormData to set multipart/form-data
